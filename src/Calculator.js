6b2e2d058d6e4d558051b39ca5c53245098db1c3
ianflynnwork <ianflynnwork@gmail.com>
2022-06-27 13:37:42 -0400

    numbers layout and display state

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,26 +1,28 @@
-function Calculator(){
+import { useState } from "react";
 
+function Calculator(){
+    const [displayValue, setDisplayValue] = useState(0);
     return (
         <div id="calculator-box">
-            <div id="return-screen"></div>
-            <div id="AC" className="calc-button">AC</div>
-            <div id="division" className="calc-button">/</div>
-            <div id="multiplication" className="calc-button">X</div>
-            <div id="subtraction" className="calc-button">-</div>
-            <div id="addition" className="calc-button">+</div>
+            <div id="display"></div>
+            <div id="clear" className="calc-button">AC</div>
+            <div id="divide" className="calc-button">/</div>
+            <div id="multiply" className="calc-button">X</div>
+            <div id="subtract" className="calc-button">-</div>
+            <div id="add" className="calc-button">+</div>
             <div id="equals" className="calc-button">=</div>
             <div className="numbers-box">
-                <div id="0" className="numbers calc-button">0</div>
-                <div id="1" className="numbers calc-button">1</div>
-                <div id="2" className="numbers calc-button">2</div>
-                <div id="3" className="numbers calc-button">3</div>
-                <div id="4" className="numbers calc-button">4</div>
-                <div id="5" className="numbers calc-button">5</div>
-                <div id="6" className="numbers calc-button">6</div>
-                <div id="7" className="numbers calc-button">7</div>
-                <div id="8" className="numbers calc-button">8</div>
-                <div id="9" className="numbers calc-button">9</div>
-                <div id="." className="numbers calc-button">.</div>
+                <div id="seven" className="numbers calc-button">7</div>
+                <div id="eight" className="numbers calc-button">8</div>
+                <div id="nine" className="numbers calc-button">9</div>
+                <div id="four" className="numbers calc-button">4</div>
+                <div id="five" className="numbers calc-button">5</div>
+                <div id="six" className="numbers calc-button">6</div>
+                <div id="one" className="numbers calc-button">1</div>
+                <div id="two" className="numbers calc-button">2</div>
+                <div id="three" className="numbers calc-button">3</div>
+                <div id="zero" className="numbers calc-button zero-width">0</div>
+                <div id="decimal" className="numbers calc-button">.</div>
             </div>
         </div>
     )
