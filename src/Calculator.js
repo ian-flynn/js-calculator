5125954387aeec9bd4fa9a0eba24f5d4ba4ececc
ianflynnwork <ianflynnwork@gmail.com>
2022-06-27 17:06:54 -0400

    adding functionality

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,27 +2,37 @@ import { useState } from "react";
 
 function Calculator(){
     const [displayValue, setDisplayValue] = useState(0);
+    
+    
+    const handleReset = () => {
+        setDisplayValue(0);
+    }
+    const handleNumber = (e) => {
+        if(displayValue !== 0){
+            setDisplayValue(displayValue + e.target.value);
+        }
+    }
     return (
         <div id="calculator-box">
-            <div id="display"></div>
-            <div id="clear" className="calc-button">AC</div>
-            <div id="divide" className="calc-button">/</div>
-            <div id="multiply" className="calc-button">X</div>
-            <div id="subtract" className="calc-button">-</div>
-            <div id="add" className="calc-button">+</div>
-            <div id="equals" className="calc-button">=</div>
+            <div id="display">{ displayValue }</div>
+            <button id="clear" className="calc-button" onClick={ handleReset }>AC</button>
+            <button id="divide" className="calc-button">/</button>
+            <button id="multiply" className="calc-button">X</button>
+            <button id="subtract" className="calc-button">-</button>
+            <button id="add" className="calc-button">+</button>
+            <button id="equals" className="calc-button">=</button>
             <div className="numbers-box">
-                <div id="seven" className="numbers calc-button">7</div>
-                <div id="eight" className="numbers calc-button">8</div>
-                <div id="nine" className="numbers calc-button">9</div>
-                <div id="four" className="numbers calc-button">4</div>
-                <div id="five" className="numbers calc-button">5</div>
-                <div id="six" className="numbers calc-button">6</div>
-                <div id="one" className="numbers calc-button">1</div>
-                <div id="two" className="numbers calc-button">2</div>
-                <div id="three" className="numbers calc-button">3</div>
-                <div id="zero" className="numbers calc-button zero-width">0</div>
-                <div id="decimal" className="numbers calc-button">.</div>
+                <button id="seven" value="7" onClick={ handleNumber } className="numbers calc-button">7</button>
+                <button id="eight" value="8" onClick={ handleNumber } className="numbers calc-button">8</button>
+                <button id="nine" value="9" onClick={ handleNumber } className="numbers calc-button">9</button>
+                <button id="four" value="4" onClick={ handleNumber } className="numbers calc-button">4</button>
+                <button id="five" value="5" onClick={ handleNumber } className="numbers calc-button">5</button>
+                <button id="six" value="6" onClick={ handleNumber } className="numbers calc-button">6</button>
+                <button id="one" value="1" onClick={ handleNumber } className="numbers calc-button">1</button>
+                <button id="two" value="2" onClick={ handleNumber } className="numbers calc-button">2</button>
+                <button id="three" value="3" onClick={ handleNumber } className="numbers calc-button">3</button>
+                <button id="zero" value="0"  onClick={ handleNumber } className="numbers calc-button zero-width">0</button>
+                <button id="decimal" value="."  onClick={ handleNumber } className="numbers calc-button">.</button>
             </div>
         </div>
     )
